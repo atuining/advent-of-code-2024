bafb5056c2c42bec47cc7a4bb195993da4a08c29
atuining <nagpalpranay@gmail.com>
2025-02-19 09:53:09 +0530

    day 1 part 2 in rust and another method for part2

--- day1.py
+++ day1.py
@@ -1,6 +1,7 @@
 from collections import Counter
 
-left, right = [], []
+left: list[int] = []
+right: list[int] = []
 
 with open("2024-1.txt", "r") as f:
     for line in f:
@@ -10,20 +11,27 @@ with open("2024-1.txt", "r") as f:
 left.sort()
 right.sort()
 
-sum = 0
+sum3 = 0
 for l, r in zip(left, right):
-    sum += l - r if l - r >= 0 else r - l
+    sum3 += l - r if l - r >= 0 else r - l
 
-print(sum)
+print(sum3)
 
-left = set(left)
+leftset = set(left)
 
-right = Counter(right)
+rightmap = Counter(right)
 
 sum2 = 0
 
-for x in left:
-    if x in right:
-        sum2 += x * right[x]
+for x in leftset:
+    if x in rightmap:
+        sum2 += x * rightmap[x]
 
 print(sum2)
+
+similarity = 0
+
+for element in left:
+    similarity += element * sum(1 for n in right if n == element)
+
+print(f"{similarity=}")
--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,4 +1,8 @@
-use std::{fs::File, io::read_to_string};
+use std::{
+    collections::{HashMap, HashSet},
+    fs::File,
+    io::read_to_string,
+};
 
 fn calculate(filename: &str) {
     let mut left: Vec<u32> = Vec::new();
@@ -7,11 +11,7 @@ fn calculate(filename: &str) {
     let file = File::open(filename).unwrap();
 
     for line in read_to_string(file).unwrap().lines() {
-        left.push(line.split_once("   ")
-            .unwrap()
-            .0
-            .parse()
-            .unwrap());
+        left.push(line.split_once("   ").unwrap().0.parse().unwrap());
         right.push(line.split_once("   ").unwrap().1.parse().unwrap());
     }
 
@@ -26,10 +26,30 @@ fn calculate(filename: &str) {
 
     let mut distance2 = 0;
 
-    
+    let mut left2 = HashSet::new();
+    for num in &left {
+        left2.insert(num);
+    }
+    let mut right2 = HashMap::new();
+    for num in &right {
+        right2.entry(num).and_modify(|val| *val += 1).or_insert(1);
+    }
 
+    for num in left2 {
+        if right2.contains_key(&num) {
+            distance2 += num * right2[&num];
+        }
+    }
+
+    println!("{}", distance2);
+
+    let mut similarity = 0;
+
+    for element in left {
+        similarity += element * (right.iter().filter(|&n| *n == element).count() as u32);
+    }
+    println!("{}", similarity);
 }
-
 
 fn main() {
     calculate("./2024-1.txt");
